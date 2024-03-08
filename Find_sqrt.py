c58555d270d8a94d3cea1b092f962bc51ba51dda
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-03-08 18:25:03 +0530

    first python program

---
+++ Find_sqrt.py
@@ -0,0 +1,26 @@
+#Soultion - 1 (using exponentiation)
+num = int(input("enter the number: "))
+
+sqrt = num ** 0.5
+
+print("The square root of ", num, "is", sqrt)
+
+#Solution - 2 (using Math module)
+
+import math
+
+num = int(input("enter the nuber: "))
+
+sqrt = math.sqrt(num)
+
+print("The square rot of", num ,"is", sqrt)
+
+#2.git init
+# 3.git add .
+# 4.git commit -m "Initial commit: Django project"
+# 5.Create a GitHub Repository
+# 6.git remote add origin your_github_repo_url.git
+# 7.git push -u origin master
+# 8.git add .gitignore
+# git commit -m "Add .gitignore" n
+# git push origin master
