4325bf23845b6387a4ddbb50803986aeda48111b
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-03-08 18:35:38 +0530

    Second problem

---
+++ Area_of_triangle.py
@@ -0,0 +1,4 @@
+Base = float(input("Enter the base: "))
+Height = float(input("Enter the height: "))
+Area = (0.5) * Base * Height
+print("The area of the triangle with base",Base,"and height",Height,"is",Area)
--- Find_sqrt.py
+++ Find_sqrt.py
@@ -15,12 +15,3 @@ sqrt = math.sqrt(num)
 
 print("The square rot of", num ,"is", sqrt)
 
-#2.git init
-# 3.git add .
-# 4.git commit -m "Initial commit: Django project"
-# 5.Create a GitHub Repository
-# 6.git remote add origin your_github_repo_url.git
-# 7.git push -u origin master
-# 8.git add .gitignore
-# git commit -m "Add .gitignore" n
-# git push origin master
