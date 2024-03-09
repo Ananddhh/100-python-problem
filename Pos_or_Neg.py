768dc96633832ddb037e1d074c0b70c6575bc462
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-03-09 13:38:48 +0530

    another  problem

---
+++ Pos_or_Neg.py
@@ -0,0 +1,9 @@
+Num = int(input("Enter a number: "))
+
+if Num > 0:
+    print(f"{Num} is positive number")
+elif Num == 0:
+    print(f"{Num} is zero")
+else:
+    print(f"{Num} is negative number")
+    
