d0a6af36035a03c0dff5d037bf30cb25de14a0c8
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-03-11 21:55:54 +0530

    another  problem

---
+++ Number_guessing.py
@@ -0,0 +1,17 @@
+import random
+
+def guess_number():
+    target_number = random.randint(1, 100)
+    attempts = 0
+    while True:
+        guess = int(input("Guess the number (between 1 and 100): "))
+        attempts += 1
+        if guess < target_number:
+            print("Too low! Try again.")
+        elif guess > target_number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number {target_number} correctly in {attempts} attempts.")
+            break
+
+guess_number()
