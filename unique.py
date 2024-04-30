36503bc84f80c7c4ce2a4c60e1d8955d02e5ec0f
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-04-30 21:52:40 +0530

    unique.py

---
+++ unique.py
@@ -0,0 +1,8 @@
+# Define a list with duplicate elements
+my_list = [1, 2, 3, 2, 4, 5, 1]
+
+# Remove duplicates using set()
+unique_list = list(set(my_list))
+
+# Print the unique list
+print(unique_list)
