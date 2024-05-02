2ab9658c900fc828cbe432c31d9d0fea6074d647
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-03 01:08:09 +0530

    random.py

--- Random.py
+++ Random.py
@@ -5,8 +5,6 @@ Num = random.randint(1,6)
 print(Num)
 
 # Example: Generate a code with a prefix and a random number
-import string
-
 def generate_custom_code(prefix, length=4):
     random_number = ''.join(random.choices(string.digits, k=length))
     return prefix + random_number
@@ -15,3 +13,14 @@ def generate_custom_code(prefix, length=4):
 custom_code = generate_custom_code("ABC")
 print(custom_code)
 
+
+import random
+import string
+
+def generate_alphanumeric_code(length=8):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choices(characters, k=length))
+
+# Generate an alphanumeric code
+alphanumeric_code = generate_alphanumeric_code()
+print(alphanumeric_code)
