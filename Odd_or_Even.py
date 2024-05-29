b0f722a164733d2938bc1ce66ea7025b56061919
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-29 19:42:32 +0530

    another commit

--- Odd_or_Even.py
+++ Odd_or_Even.py
@@ -7,3 +7,12 @@ elif Num % 2 == 1:
 else:
     print("Not an integer")
     
+# another way
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+even_numbers = [num for num in num_list if num % 2 == 0]
+odd_numbers = [num for num in num_list if num % 2 != 0]
+
+print("Even numbers:", even_numbers)
+print("Odd numbers:", odd_numbers)
