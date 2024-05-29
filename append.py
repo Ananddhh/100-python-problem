15c1b22c271891f7b59fc4199d2d9524e0c7e4e7
Anandhu kb <157713213+Ananddhh@users.noreply.github.com>
2024-05-29 19:40:13 +0530

    another commit

---
+++ append.py
@@ -0,0 +1,7 @@
+num_list = [1, 2, 3, 4, 5]
+
+# Create a new list to store the squares
+squares_list = [num ** 2 for num in num_list]
+
+# Print the new list with squares
+print("List of squares:", squares_list)
